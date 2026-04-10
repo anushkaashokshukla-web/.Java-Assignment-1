6eecf07d1bbe63a5afc6ba2afd29ba35e2a6d015
anushkaashokshukla-web <anushkaashokshukla@gmail.com>
2026-04-10 13:08:59 +0530

    Implement basic MusicPlayer class with threading

---
+++ MusicPlayer.java
@@ -0,0 +1,15 @@
+class MusicPlayer extends Thread {
+    public void run() {
+        try {
+            System.out.println("Playing song...");
+            for (int i = 1; i <= 3; i++) {
+                Thread.sleep(1000);
+                System.out.println("Progress " + i);
+            }
+        } catch (Exception e) {}
+    }
+
+    public static void main(String[] args) {
+        new MusicPlayer().start();
+    }
+}
